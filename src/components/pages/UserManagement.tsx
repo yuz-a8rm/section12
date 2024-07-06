9431c6f56f175eb65b0adb0508b06f4381e05a02
yuz-a8rm <167948100+yuz-a8rm@users.noreply.github.com>
2024-07-06 15:15:53 +0000

    77まで

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,17 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "./organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://picsum.photos/800"
+          userName="じゃけぇ"
+          fullName="Takumi Okada"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
